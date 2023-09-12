273a50378c84cbdb9e216e7381da94fd0be449a7
kronborgftp <frpa0001@stud.kea.dk>
2023-09-12 16:37:53 +0200

    SUPERHERO DEL 2: Added switch case instead of while loop.

--- src/Main.java
+++ src/Main.java
@@ -13,46 +13,62 @@ public class Main {
         Controller controller = new Controller(database);
 
         System.out.println("-------Welcome to the SUPERHERO UNIVERSE------");
+
+        int choice = 0;
+
         System.out.println("1. Add superhero \n" +
-                "9. Exit the program");
-        String choice = keyboard.nextLine().toLowerCase();
+                "2. Show list of superheroes \n" +
+                "3. Exit the program");
 
+        choice = keyboard.nextInt();
 
-        while (choice.equals("1")) {
-            System.out.println("Add superhero by typing its name! ");
-            String name = keyboard.nextLine();
+        //loops around until the user chooses to exit the program.
+        while (choice != 3) {
+            // Using a "Switch Case" instead of while loop.
+            switch (choice) {
+                case 1:
+                    System.out.println("Add superhero by typing its superhero name: ");
+                    keyboard.nextLine();
+                    String name = keyboard.nextLine();
 
-            System.out.println("Enter the real name of your superhero!: ");
-            String realName = keyboard.nextLine();
+                    System.out.println("Enter the real name of your superhero!: ");
+                    String realName = keyboard.nextLine();
 
-            System.out.println("Enter the amazing superpower of your superhero!: ");
-            String superpower = keyboard.nextLine();
+                    System.out.println("Enter the amazing superpower of your superhero: ");
+                    String superpower = keyboard.nextLine();
 
-            System.out.println("Enter what year your superhero was created!: ");
-            int yearCreated = keyboard.nextInt();
+                    System.out.println("Enter what year your superhero was created: ");
+                    int yearCreated = keyboard.nextInt();
 
-            System.out.println("Is your superhero human? (true/false): ");
-            boolean isHuman = keyboard.nextBoolean();
+                    System.out.println("Is your superhero human? (true/false): ");
+                    boolean isHuman = keyboard.nextBoolean();
 
-            System.out.println("Enter the strength of your superhero in numbers!: ");
-            double strength = keyboard.nextDouble();
+                    System.out.println("Enter the strength of your superhero in numbers: ");
+                    double strength = keyboard.nextDouble();
 
-            //adder superheroen til databasen.
-            controller.addSuperhero(name, realName, superpower, yearCreated, isHuman, strength);
+                    //Adds the new superhero to the database
+                    controller.addSuperhero(name, realName, superpower, yearCreated, isHuman, strength);
+                    break;
 
-            System.out.println("Do you want to add another superhero? (yes/no)");
-            keyboard.nextLine(); //Sikrer os at programmet ser næste line. Programmet ville slutte ellers
-            choice = keyboard.nextLine().toLowerCase();
-        }
+                case 2:
+                    //Allows the user to display a list of the superheroes, without adding any
+                    List<Superhero> superheroList = controller.getAllSuperheroes();
+                    System.out.println("All the amazing superheroes: ");
+                    for (Superhero superhero : superheroList) {
+                        if (superhero != null) {
+                            System.out.println(superhero);
+                        }
+                    }
+                    break;
 
-        //Henter alle superheroes fra databasen
-        List<Superhero> superheroList = controller.getAllSuperheroes();
+                //allows the user to exit the program
+                case 3:
+                    System.out.println("Have a nice day.");
+                    break;
 
-        //Udskriver Superhero objekter
-        System.out.println("All the amazing superheroes!:");
-        for (Superhero superhero : superheroList) {
-            if (superhero != null) {
-                System.out.println(superhero);
+                default:
+                    System.out.println("Please choose one of the option listed above.");
+                    break;
             }
         }
     }
