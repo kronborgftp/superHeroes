5466f63d50f979a67dc5328cbea479d7c05c8c71
kronborgftp <frpa0001@stud.kea.dk>
2023-09-18 10:37:31 +0200

    fail

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     private Controller controller;
 
     public static void main(String[] args) {
-        UserInterface ui = new UserInterface(System.in);
+        UserInterface ui = new UserInterface();
         ui.startProgram();
     }
 }
