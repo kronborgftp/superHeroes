3651dc49d2c2b363f68474c8f46482756f5813a9
kronborgftp <frpa0001@stud.kea.dk>
2023-11-10 15:13:41 +0100

    main change

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 
 public class Main {
-
-    private Controller controller;
-
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
         ui.startProgram();
