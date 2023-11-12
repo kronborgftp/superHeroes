af11cef8cd970ac8a2aa3ceddbee346db1bc64e7
kronborgftp <frpa0001@stud.kea.dk>
2023-11-12 13:24:54 +0100

    unit test fix

--- test/DatabaseTest.java
+++ test/DatabaseTest.java
@@ -1,7 +1,6 @@
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Test;
 
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
